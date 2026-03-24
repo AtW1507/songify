1670ad4a7eb57227bc22631acef50c648e4d13d0
Adrian Wilczek <adrian.wilczek@onet.com.pl>
2026-03-24 08:57:54 +0100

    Added a custom OpenSSL certificate

--- src/main/java/com/songify/infrastructure/security/SecurityUser.java
+++ src/main/java/com/songify/infrastructure/security/SecurityUser.java
@@ -34,17 +34,17 @@ class SecurityUser implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
